a8c198ed29e312caa15cc8c240064eea42f270d8
Chris <cpaterson152@gmail.com>
2023-05-16 13:06:18 -0700

    Update questions for great accuracy

--- questions.js
+++ questions.js
@@ -74,16 +74,16 @@ let questions = [
     {
         number: 9,
         question: "Which of the following is NOT true of OOP in JavaScript?",
-        a: "JS methods defined as properties within an object is an example of encapsulation",
-        b: "You cannot create static methods in JavaScript",
-        c: "JavaScript objects use prototypal inheritance",
-        d: "Polymorphism allows us to have custom code on each instance of an object, instead of long switch/case statements on the parent object",
+        a: "Access control, abstracting away internal details, and having methods defined as properties within an object are all examples of encapsulation",
+        b: "JavaScript does not support the creation of static methods directly within a class declaration",
+        c: "JavaScript objects use prototypal inheritance, meaning they inherit directly from other objects (their prototypes)",
+        d: "Polymorphism allows objects of different classes to be treated as instances of a common interface or shared set of methods, which may or may not be defined within a superclass",
         correct: "b"
     },
     {
         number: 10,
         question: "Which of the following is NOT true about JavaScript in general?",
-        a: "With the addition of Worker Threads, typically single-threaded JS becomes both parallel and concurrent",
+        a: "With the addition of Worker Threads, typically single-threaded JS becomes fully parallel",
         b: "JavaScript supports asynchronous programming with callback functions, promises, asynch, and await",
         c: "JS has APIs for working with text, dates, regular expressions, standard data structures, and the DOM",
         d: "Executed JavaScript creates an execution context that consists of a Variable Environment and a Thread of Execution",
